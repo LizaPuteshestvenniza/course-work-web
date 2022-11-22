0ee198a4ec18095d6b95a33c1ce91ff6c20d58f8
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-22 21:01:37 +0300

    Add files via upload

---
+++ course-work/src/firebase.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+const firebaseConfig = {
+    apiKey: "AIzaSyAZtxh4OcQe3eZ9L0ZdEBSPgnBXaiX8DVo",
+    authDomain: "phone-shop-84650.firebaseapp.com",
+    projectId: "phone-shop-84650",
+    storageBucket: "phone-shop-84650.appspot.com",
+    messagingSenderId: "131463443423",
+    appId: "1:131463443423:web:0390e09755faf800d38a6a",
+    measurementId: "G-P03PGDLXX6"
+};
