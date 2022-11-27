1cfb296fae3b368779c1228e1494017cab31bf45
Anastasias35 <71524176+Anastasias35@users.noreply.github.com>
2022-11-27 22:56:52 +0300

    Update Header.js

--- course-work/src/Header.js
+++ course-work/src/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Header.css';
-
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import {Link} from "react-router-dom";
 
 function Header() {
 	return (
 		<div className='header'>
-			<img class='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG10.png' />
+			<Link to="/">
+				<img className='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG10.png'/>
+			</Link>
 			<div className='header_search'>
-				<input className='header_searchInput' type='text' />	
+				<input className='header_searchInput' type='text' />
+			<SearchIcon className="header_searchIcon"/>
+
 			</div>
 			<div className='header_nav'>
 				<div className='header_option'>
@@ -34,6 +40,14 @@ function Header() {
 						Prime
 					</span>
 				</div>
+				<Link to="/checkout">
+					<div className="header_optionBasket">
+						<ShoppingBasketIcon />
+						<span className="header_optionLineTwo header_BasketCount">0</span>
+					</div>
+				</Link>
+
+
 			</div>
 		</div>
 
