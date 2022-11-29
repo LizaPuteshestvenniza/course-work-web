88a440fa97ad06cb2bb332eaf07ed3833d91bd6d
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-30 00:24:20 +0300

    Update App.js

--- course-work/src/App.js
+++ course-work/src/App.js
@@ -1,22 +1,43 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from "./Checkout";
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-
+import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> ', authUser);
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
-      <Router>
-          <div className="App">
-              <Header />
-              <Routes>
-                  <Route path="/"  element={<Home />}/> {/*add element={<Home />} */}
-                  <Route path="/checkout" element={<Checkout />}/>
-                  {/*<Route path="/login" element{}/>*/}
-              </Routes>
-          </div>
-      </Router>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<><Header/><Home/></>} />
+          <Route path="/checkout" element={<><Header/><Checkout /></>} />
+          <Route path="/login" element={<Login/>} />
+          {/*<Route path="/login" element{}/>*/}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
