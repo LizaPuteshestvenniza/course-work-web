eb2dc75c1864777d2dbd5bc83a5110a4db1ef578
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-30 00:28:33 +0300

    Update Header.js

--- course-work/src/Header.js
+++ course-work/src/Header.js
@@ -4,33 +4,40 @@ import logo from './logoCW.png';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import {Link} from "react-router-dom";
-import {useStateValue} from "./StateProvider";
-
+import { useStateValue } from "./StateProvider";
+import { auth } from './firebase';
 
 function Header() {
 
-	const [{basket},dispatch] = useStateValue();
+	const [{ basket, user }, dispatch] = useStateValue();
+	const handleAuthentication = () => {
+		if (user) {
+			auth.signOut();
+		}
+	}
 
 	return (
 		<div className='header'>
 			<Link to="/">
-
-				<img className='header_logo' src={logo}/>
+				<img className='header_logo' src={logo} alt='logo'/>
 			</Link>
+
 			<div className='header_search'>
 				<input className='header_searchInput' type='text' />
 				<SearchIcon className="header_searchIcon"/>
 			</div>
 
 			<div className='header_nav'>
-				<div className='header_option'>
-					<span className='header_optionLineOne'>
-						Hello, Guest
-					</span>
-					<span className='header_optionLineTwo'>
-						Sign In
-					</span>
-				</div>
+				<Link to={!user && '/login'}>
+					<div onClick={handleAuthentication} className='header_option'>
+						<span className='header_optionLineOne'>
+							Hello, Guest
+						</span>
+						<span className='header_optionLineTwo'>{user ? 'Sign Out' : 'Sign In'}
+						</span>
+					</div>
+				</Link>
+
 				<div className='header_option'>
 					<span className='header_optionLineOne'>
 						Returns
