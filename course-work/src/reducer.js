8221d4c92e38a563dab907083bf6a8d7ba99a21b
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-30 00:25:18 +0300

    Update reducer.js

--- course-work/src/reducer.js
+++ course-work/src/reducer.js
@@ -1,10 +1,10 @@
 export const initialState = {
-    basket:[]
+    basket: [],
+    user: null
 };
 
 // Selector
 export const getBasketTotal = (basket) => basket?.reduce((amount,item)=> item.price + amount,0);
-
 
 const reducer = (state,action) => {
     console.log(action);
@@ -25,6 +25,13 @@ const reducer = (state,action) => {
                 ...state,
                 basket:newBasket
             }
+        
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+        
         default:
             return state;
     }
