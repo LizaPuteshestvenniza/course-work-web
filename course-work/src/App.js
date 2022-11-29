862468b4eed96331723ca9a1e846f537e643d4c0
Anastasias35 <asypacheva35@gmail.com>
2022-11-29 19:55:24 +0300

    add Checkout Page

--- course-work/src/App.js
+++ course-work/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from "./Checkout";
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+      <Router>
+          <div className="App">
+              <Header />
+              <Routes>
+                  <Route path="/"  element={<Home />}/> {/*add element={<Home />} */}
+                  <Route path="/checkout" element={<Checkout />}/>
+                  {/*<Route path="/login" element{}/>*/}
+              </Routes>
+          </div>
+      </Router>
   );
 }
 
--- course-work/src/Header.js
+++ course-work/src/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './Header.css';
 import logo from './logoCW.png';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import {Link} from "react-router-dom";
+import {useStateValue} from "./StateProvider";
 
 
 function Header() {
+
+	const [{basket},dispatch] = useStateValue();
+
 	return (
 		<div className='header'>
-			<img class='header_logo' src={logo}/>
+			<Link to="/">
+
+				<img className='header_logo' src={logo}/>
+			</Link>
 			<div className='header_search'>
-				<input className='header_searchInput' type='text' />	
+				<input className='header_searchInput' type='text' />
+				<SearchIcon className="header_searchIcon"/>
 			</div>
+
 			<div className='header_nav'>
 				<div className='header_option'>
 					<span className='header_optionLineOne'>
@@ -35,6 +47,12 @@ function Header() {
 						Prime
 					</span>
 				</div>
+				<Link to="/checkout">
+					<div className="header_optionBasket">
+						<ShoppingBasketIcon />
+						<span className="header_optionLineTwo header_BasketCount">{basket.length}</span>
+					</div>
+				</Link>
 			</div>
 		</div>
 
